814aa44f2e7086fbceb6a1d686f0df69dcae4bfc
HanYagang <70358969@qq.com>
2021-10-17 19:40:41 +0800

    .feat: 增加更新检查

---
+++ utils/get-pkg-info/README.md
@@ -0,0 +1,11 @@
+# `@eqshow/get-pkg-info`
+
+> TODO: description
+
+## Usage
+
+```
+const getPkgInfo = require('@eqshow/get-pkg-info');
+
+// TODO: DEMONSTRATE API
+```
---
+++ utils/get-pkg-info/lib/index.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const urlJoin = require('url-join')
+const axios = require('axios')
+const { semver } = require('@eqshow/shared')
+
+// 获取包相关信息
+function getPkgInfo(pkgName, registry) {
+  if (!pkgName) return null
+  // 获取源路径 npm源 或者 taobao源
+  const registryUrl = registry || getDefaultRegistry()
+  const pkgInfoUrl = urlJoin(registryUrl, pkgName)
+  return axios.get(pkgInfoUrl)
+    .then(res => {
+      if (res.status === 200 && res.data) {
+        return res.data
+      }
+      return null
+    })
+}
+
+// 获取默认registry
+function getDefaultRegistry(isOriginal = false) {
+  return isOriginal ? 'https://registry.npmjs.org' : 'https://registry.npm.taobao.org'
+}
+
+// 获取包的所有版本
+async function getPkgVersions(pkgName, registry) {
+  const data = await getPkgInfo(pkgName, registry)
+  return data ? Object.keys(data.versions) : []
+}
+
+// 获取当前主版本号下，大于当前版本的所有版本
+async function getPkgSemverVersions(pkgName, pkgVersion, registry) {
+  const versions = await getPkgVersions(pkgName, registry)
+  return versions
+    .filter(version => semver.satisfies(version, `^${pkgVersion}`))
+    .sort((a, b) => semver.lt(a, b) ? 1 : -1)
+}
+
+// 获取当前主版本号下，最新的版本号
+async function getPkgSemverVersion(pkgName, pkgVersion, registry) {
+  const semverVersions = await getPkgSemverVersions(pkgName, pkgVersion, registry)
+  return semverVersions[0]
+}
+
+module.exports = {
+  getDefaultRegistry,
+  getPkgInfo,
+  getPkgVersions,
+  getPkgSemverVersions,
+  getPkgSemverVersion
+}
