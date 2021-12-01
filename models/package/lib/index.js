563ef8aa47ebb5c2c1cb9d77634c24bd1fde61ff
HanYagang <70358969@qq.com>
2021-12-01 15:13:20 +0800

    .feat: package模块

--- models/package/lib/index.js
+++ models/package/lib/index.js
@@ -3,11 +3,14 @@
 const path = require('path')
 const pkgDir = require('pkg-dir').sync
 const npminstall = require('npminstall')
+const pathExists = require('path-exists').sync
+const fse = require('fs-extra')
+const { getDefaultRegistry, getPkgLatestVersion } = require('@eqshow/get-pkg-info')
 const {
   isObject,
   normalizeFilePath
 } = require('@eqshow/shared')
-const { getDefaultRegistry } = require('@eqshow/get-pkg-info')
+const log = require('@eqshow/log')
 
 class Package {
   constructor(options) {
@@ -23,33 +26,57 @@ class Package {
     this.version = options.version
     // pkg的指定路径（绝对路径）
     this.targetPath = options.targetPath
+    // 缓存模式/指定模式
+    this.cache = options.cache
   }
 
-  // pkg的存储路径
+  // 获取缓存文件名称前缀
+  get cacheFilePrefix() {
+    return this.name.replace(/\//, '_')
+  }
+
+  // 获取缓存地址 
   get storagePath() {
-    //
-    return 'storage path'
+    if (this.cache) {
+      return path.resolve(this.targetPath, `node_modules/_${'@vue_cli'}@${this.version}@${'@vue/cli'}`)
+    }
+    return null
   }
 
-  // 判断pkg是否存在
-  exists() {
-    let targetPath = process.env.EQX_CLI_TARGET_PATH
-    if (targetPath) {
-      console.log('指定模式')
-    } else {
-      console.log('缓存模式')
+  async prepare() {
+    // 缓存目录不存在则创建目录
+    if (this.cache && !pathExists(this.targetPath)) {
+      fse.mkdirpSync(this.targetPath)
     }
+    // 获取版本号
+    if (this.version === 'latest') {
+      try {
+        this.version = await getPkgLatestVersion('@vue/cli')
+      } catch (error) {
+        log.error(error.message)
+      }
+    }
+  }
+
+  // 判断缓存的pkg是否存在
+  async exists() {
+    if (this.cache) {
+      await this.prepare()
+      return pathExists(this.storagePath)
+    }
+    return false
   }
 
   // 安装pkg
   install() {
     return npminstall({
+      registry: getDefaultRegistry(),
       root: this.targetPath,
       storeDir: path.resolve(this.targetPath, 'node_modules'),
-      registry: getDefaultRegistry(),
-      pkgs: [
-        { name: 'vue', version: '~2.6.13' },
-      ],
+      pkgs: [{
+        name: '@vue/cli',
+        version: this.version
+      }],
     })
   }
 
@@ -60,12 +87,13 @@ class Package {
   getRootFilePath() {
     // 1、获取package.json所在目录
     const dir = pkgDir(this.targetPath)
-    if (!dir) return null
-    // 2、读取package.json
-    const pkg = require(path.resolve(dir, 'package.json'))
-    // 3、返回格式化之后的包入口文件路径
-    if (pkg && pkg.main) {
-      return normalizeFilePath(path.resolve(dir, pkg.main))
+    if (dir) {
+      // 2、读取package.json
+      const pkg = require(path.resolve(dir, 'package.json'))
+      // 3、返回格式化之后的包入口文件路径
+      if (pkg && pkg.main) {
+        return normalizeFilePath(path.resolve(dir, pkg.main))
+      }
     }
     return null
   }
