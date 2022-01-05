104129a3da1bb741943ab373cd93de124f01a4fa
hanyagang <70358969@qq.com>
2022-01-06 00:06:54 +0800

    feat: 完善invoke方法

--- models/generator/lib/generator-api.js
+++ models/generator/lib/generator-api.js
@@ -1,17 +1,70 @@
+const path = require("path")
+
+const fse = require("fs-extra")
+const globby = require("globby")
+const pkgDir = require("pkg-dir").sync
+const ejs = require("ejs")
+
 class GeneratorAPI {
   /**
-   * @param {string} id - Id of the owner plugin
-   * @param {Generator} generator - The invoking Generator instance
-   * @param {object} options - generator options passed to this plugin
+   * @param {string} id - 插件名称
+   * @param {Generator} generator - Generator实例
+   * @param {object} options - generator配置
    */
   constructor(id, generator, options) {
     this.id = id
     this.generator = generator
     this.options = options
+    // 获取 package.json 所在的工作目录
+    const baseDir = pkgDir(process.cwd())
+    this.baseDir = baseDir
   }
 
-  render(src, options) {
-    console.log(src, options)
+  // 注入回调
+  _injectCallBacks(cb) {
+    this.generator.callbacks.push(cb)
+  }
+
+  async render(src) {
+    const obj = {}
+    Error.captureStackTrace(obj)
+    const callSite = obj.stack.split("\n")[2]
+
+    // the regexp for the stack when called inside a named function
+    const namedStackRegExp = /\s\((.*):\d+:\d+\)$/
+    // the regexp for the stack when called inside an anonymous
+    const anonymousStackRegExp = /at (.*):\d+:\d+$/
+
+    let matchResult = callSite.match(namedStackRegExp)
+    if (!matchResult) {
+      matchResult = callSite.match(anonymousStackRegExp)
+    }
+
+    // 获取 render 指定所在的目录
+    const source = path.resolve(path.dirname(matchResult[1]), src)
+
+    // 收集回调 -> ejs渲染文件
+    this._injectCallBacks(async () => {
+      // 获取所有文件路径
+      const files = await globby("**", {
+        cwd: source,
+        ignore: ["node_modules/**", "public/**", "yarn.lock"],
+        nodir: true
+      })
+
+      // 渲染所有文件
+      files.forEach((file) => {
+        // 模板所在的完整路径
+        const absolutePath = path.resolve(source, file)
+        ejs.renderFile(absolutePath, {}, {}, (err, result) => {
+          if (err) throw Error(err)
+          // 要被渲染的完整路径
+          const filePath = path.resolve(this.baseDir, file)
+          fse.ensureDirSync(path.dirname(filePath))
+          fse.writeFileSync(filePath, result)
+        })
+      })
+    })
   }
 }
 
--- models/generator/lib/index.js
+++ models/generator/lib/index.js
@@ -6,18 +6,32 @@ class Generator {
     this.context = context
     // 当前安装的插件信息
     this.plugin = plugin
+    // 收集回调 -> 解决插件中执行异步渲染等的问题
+    this.callbacks = []
   }
 
+  /**
+   * 初始化插件信息
+   * 1. 调用回调函数
+   */
   async initPlugins() {
-    // eslint-disable-next-line no-unused-vars
     const { id, apply, options } = this.plugin
-    // eslint-disable-next-line no-unused-vars
     const api = new GeneratorAPI(id, this, options)
-    // await apply(api, options)
+    await apply(api, options)
+  }
+
+  // 清空收集的回调队列
+  async flushCallbacks() {
+    for (const callback of this.callbacks) {
+      await callback()
+    }
   }
 
   async generate() {
+    // 收集插件的render回调
     await this.initPlugins()
+    // 清空回调队列
+    await this.flushCallbacks()
   }
 }
 
